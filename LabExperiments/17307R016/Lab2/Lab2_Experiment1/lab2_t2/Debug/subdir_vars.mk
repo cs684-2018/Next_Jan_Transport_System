b08b58e88ccd4f9bc47b59d9947342e2685c89cb
Punit Jain <punitjain8695@gmail.com>
2018-05-01 01:14:33 +0530

    Add files via upload

---
+++ LabExperiments/17307R016/Lab2/Lab2_Experiment1/lab2_t2/Debug/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+/opt/ti/TivaWare_C_Series-2.1.2.111/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../lab2_t2.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_DEPS += \
+./lab2_t2.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lab2_t2.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"lab2_t2.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"lab2_t2.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../lab2_t2.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
